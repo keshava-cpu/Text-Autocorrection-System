0621108be1e7c47d98424afba7c9ebcd02743044
Keshava <keshavasomarouthu41@gmail.com>
2025-12-26 08:22:56 +0530

    Adding Levenshtein Distance computation into the file structure, furture improvements are needed.

---
+++ levenshtein_distance_calc.py
@@ -0,0 +1,9 @@
+def lev(a, b):
+    if (len(a) == 0 or len(b) == 0): return len(a)
+    if (a[0] == b[0]): return lev(a[1:], b[1:])
+    else:
+        return (1 + min(lev(a, b[1:]), lev(a[1:], b), lev(a[1:], b[1:])))
+
+a, b = input().strip().split()
+print(a, b)
+print(lev(a, b))
