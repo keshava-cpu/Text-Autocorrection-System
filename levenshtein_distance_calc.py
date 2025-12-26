df447d5a8b76122ffd4ca22ec4b670b3d6f25405
Keshava <keshavasomarouthu41@gmail.com>
2025-12-26 09:05:40 +0530

    Modified Levenshtein Distance Algorithm logic

--- levenshtein_distance_calc.py
+++ levenshtein_distance_calc.py
@@ -1,5 +1,6 @@
 def lev(a, b):
-    if (len(a) == 0 or len(b) == 0): return len(a)
+    if (len(a) == 0): return len(b)
+    if (len(b) == 0): return len(a)
     if (a[0] == b[0]): return lev(a[1:], b[1:])
     else:
         return (1 + min(lev(a, b[1:]), lev(a[1:], b), lev(a[1:], b[1:])))
